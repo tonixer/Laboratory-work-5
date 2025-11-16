5e3032c02beb4aa5ad44b07b9df3dbc2d425798f
tonixer <tonixplay9@gmail.com>
2025-11-16 22:02:17 +0100

    1

--- Task 454.py
+++ Task 454.py
@@ -1,8 +1,8 @@
 numbers = [int(num) for num in input("Введіть числа через пробіл: ").split()]
-odd_numbers = set()
+odd_numbers = []
 
 for number in numbers:
     if not number % 2 == 0:
-        odd_numbers.add(number)
+        odd_numbers.append(number)
 
 print(list(odd_numbers))
